2b258fd4a683368260586691864e09e1016f46d7
Yilmaz Edis y84185251 <yilmaz.edis@huawei.com>
2022-02-17 11:01:59 +0300

    Added hero image ver 3

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -24,7 +24,7 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
 
-        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
     }
 
     override func viewDidLayoutSubviews() {
